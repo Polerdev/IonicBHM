88248fbb145d3ef1e21b1fdee1abdbd482419feb
polerdev <poleredev@gmail.com>
2024-12-06 20:08:02 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'uno',
+  webDir: 'www'
+};
+
+export default config;
